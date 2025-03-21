412622b05ba0914730e49ece4c903cebcbaa6d77
ZaynebMegdich1 <zeyneb.megdich@gmail.com>
2025-03-21 13:35:09 +0100

    Update Analysis.sql

--- Analysis.sql
+++ Analysis.sql
@@ -99,18 +99,17 @@ SELECT
     pr.product_name,
     SUM(pr.price) AS total_price,
     SUM(f.spend_amount) AS total_spend,
-	pr.inventory AS product_inventory,
-	SUM(f.impressions) AS total_impressions,
-	SUM(f.clicks) AS total_clicks,
-	SUM(f.conversions) AS total_conversions,
+    SUM(f.impressions) AS total_impressions,
+    SUM(f.clicks) AS total_clicks,
+    SUM(f.conversions) AS total_conversions,
     CAST((SUM(f.conversions) * 100.0 / NULLIF(SUM(f.impressions), 0)) AS DECIMAL(18,3)) AS conversion_rate,
-	CAST(SUM(CAST(f.spend_amount AS DECIMAL(18,2))) / NULLIF(SUM(CAST(f.conversions AS DECIMAL(18,2))), 0) AS DECIMAL(18,2)) AS cost_per_conversion
+    CAST(SUM(CAST(f.spend_amount AS DECIMAL(18,2))) / NULLIF(SUM(CAST(f.conversions AS DECIMAL(18,2))), 0) AS DECIMAL(18,2)) AS cost_per_conversion
 FROM 
     gold.dim_product pr
 INNER JOIN gold.fact f ON pr.product_id = f.product_id
 INNER JOIN gold.dim_campaign c ON f.campaign_id = c.campaign_id
 GROUP BY 
-    pr.product_name, pr.inventory
+    pr.product_name
 ORDER BY 
     conversion_rate DESC;
 
